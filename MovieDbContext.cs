12988f9d50530004410c0ebf302b810309dee4ac
Ali Rafay <alirafaybhojani2001@gmail.com>
2023-09-13 13:48:59 +0500

    init

---
+++ MovieDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.WebAPI.Entities;
+
+namespace Movies.WebAPI
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {}
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
